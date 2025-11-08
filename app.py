eaa40468ebb56519f429dc80c21df298a082ec17
rohithshan23 <rohithshan23@gmail.com>
2025-11-08 14:12:58 +0530

    Remove firebase-key.json from tracking

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-from firestore_utils import add_expense
+from flask import Flask, render_template, request, redirect
+from firestore_utils import add_expense, get_expenses
 from datetime import datetime
 
 app = Flask(__name__)
@@ -25,7 +25,19 @@ def submit():
     }
 
     add_expense(data)
-    return "Expense added successfully!"
+    return redirect("/")
+    # return """
+    #     <h3>Expense logged successfully!</h3>
+    #     <a href="/">Go Back</a>
+    #     """
+
+@app.route("/get_expenses", methods=["GET"])
+def fetch_expenses():
+    try:
+        expense_list = get_expenses()
+        return render_template("expenses.html", expenses=expense_list)
+    except Exception as e:
+        return f"Error: {e}"
 
 if __name__ == "__main__":
     app.run(debug=True)
